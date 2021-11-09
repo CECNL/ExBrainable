6065a4c646fab5d6acacde554bf3b1803c578dc3
bai06hua28lin.bt06@nctu.edu.tw <yalinhuang.bt06@nycu.edu.tw>
2021-11-09 09:53:14 +0800

    add cross subject study

--- ExBrainable/GUI.py
+++ ExBrainable/GUI.py
@@ -1,9 +1,11 @@
 # Authors: Ya-Lin Huang <yalinhuang.bt06@nycu.edu.tw>
 
+from operator import concat
 import tkinter as tk
 from tkinter.constants import Y
 from tkinter.ttk import *
 from tkinter import messagebox
+from mne import event
 from torchsummary import summary
 
 import threading
@@ -93,6 +95,7 @@ def main():
         Model Selection
         Training Setting
         Model Training
+        Model Summary 
     Results
         Model Prediction
         Model Performance
@@ -108,7 +111,8 @@ def main():
     filemenu = tk.Menu(menubar, tearoff=0)
     menubar.add_cascade(label='File ', menu=filemenu)
     filemenu.add_command(label='Load Data', command=lambda:Load_dataset())
-    filemenu.add_command(label='Load Model Weight', command=Weightname)
+    filemenu.add_command(label='Load Model Weight', command=lambda:Weightname)
+    filemenu.add_command(label='Study(cross subject)', command=lambda:load_files())
 
     # Menu-Model
     Model = tk.Menu(menubar, tearoff=0)
@@ -116,7 +120,7 @@ def main():
     Model.add_command(label='Model Selection', command=lambda:load_model_struct())
     Model.add_command(label='Training Setting', command=lambda:Model_Preparation())
     Model.add_command(label='Model Training', command=(lambda:bar()))
-    
+    Model.add_command(label='Model Summary', command=(lambda:full_model_summary()))
     #Menu- Results 
     
     Results = tk.Menu(menubar, tearoff=0)
@@ -133,6 +137,7 @@ def main():
     Model.entryconfig("Model Selection", state="disabled")
     Model.entryconfig("Training Setting", state="disabled")
     Model.entryconfig("Model Training", state="disabled")
+    Model.entryconfig("Model Summary", state="disabled")
     Results.entryconfig("Model Performance", state="disabled")
     Results.entryconfig("Model Prediction", state="disabled")
     Results.entryconfig("Model Interpretation", state="disabled")
@@ -205,20 +210,25 @@ def path2entry(entry, set_type):
     
 
 def read_path(path, set_type):
+    global ftype
     try: 
         if '.mat' in path:
             data_ = scipy.io.loadmat(path)
             if set_type not in data_.keys():
                 raise ValueError (f'Dict key must be {set_type}.')
             data= data_[set_type]
+            ftype= '.mat'
+
         if '.set' in path: 
-            data = mne.io.read_epochs_eeglab(path)
-            data =data._data
-            
+            data = mne.io.read_epochs_eeglab(path, uint16_codec='latin1')
+            #data =data._data
+            ftype='.set'
+
         if '.edf' in path: 
             data= mne.io.read_raw_edf(path,preload= True)
             data =data._data
-            
+            ftype='.edf'
+
         return data     
 
     except TypeError:
@@ -375,8 +385,310 @@ def model_select(win_lms, modelist):
 
     filemenu.entryconfig("Load Model Weight", state= 'normal')
     Model.entryconfig("Training Setting", state="normal")
-
+    
     win_lms.destroy()
+
+# File- study
+
+
+def load_files():
+    global btn
+    w_files=new_window(Title='Cross subject study', size='500x450', x_pad=-10, y_pad=10)
+
+    xframes = tk.LabelFrame(w_files, text="Data", height=10, bg='White', width=50, labelanchor='n')
+    xframes.pack(fill='both', ipadx=1 ,ipady= 10,padx=20 ,pady= 18)
+    yframes = tk.LabelFrame(w_files, text="Labels", height=10, bg='White', width=50, labelanchor='n')
+    yframes.pack(fill='both', ipadx=1 ,ipady= 10,padx=20 ,pady= 18)
+    # xtrainframes = tk.LabelFrame(w_files, text="Training Data", height=10, bg='White', width=50, labelanchor='n')
+    # xtrainframes.pack(fill='both', ipadx=1 ,ipady= 10,padx=20 ,pady= 18)
+    # ytrainframes = tk.LabelFrame(w_files, text="Training Label", height=10, bg='White', width=50, labelanchor='n')
+    # ytrainframes.pack(fill='both', ipadx=1 ,ipady= 10,padx=20 ,pady= 18)
+    # xtestframes = tk.LabelFrame(w_files, text="Test Data", height=10, bg='White', width=50, labelanchor='n')
+    # xtestframes.pack(fill='both', ipadx=1 ,ipady= 10,padx=20 ,pady= 18)
+    # ytestframes = tk.LabelFrame(w_files, text="Test Label", height=10, bg='White', width=50, labelanchor='n')
+    # ytestframes.pack(fill='both', ipadx=1 ,ipady= 10,padx=20 ,pady= 18)
+
+    #load subjects files
+    ttk.Button(xframes, text='Add Files', command=(lambda:read_data_path(xframes, 'x_train')), width=10).pack()
+    ttk.Button(yframes, text='Add Files', command=(lambda:read_data_path(yframes, 'y_train')), width=10).pack()
+    # ttk.Button(xtestframes, text='Add Files', command=(lambda:read_data_path(xtestframes, 'x_test')), width=10).pack()
+    # ttk.Button(ytestframes, text='Add Files', command=(lambda:read_data_path(ytestframes, 'y_test')), width=10).pack()
+    
+
+    Confirm = ttk.Button(w_files ,text="Confirm", command=(lambda:rename_events(w_files)), width=10).pack()
+
+def read_data_path(frame, set_type):
+    global concat_data, event_ids, subname, sublen
+    # identify pathtypes: multiple sub files are named as 'files_x_train','files_y_train'...
+    files = tk.filedialog.askopenfilenames()
+    fnum=1
+    event_ids=[]
+    subname=[]
+    sublen=[]
+
+    for path in files:
+        # name each path: 'x_trainpath_1','x_trainpath_2'
+        globals()[str(set_type)+'path_'+str(fnum)]= path
+
+        # read and each file:'xtrain_1', 'xtrain_2' 
+        globals()[str(set_type)+'_'+str(fnum)]= read_path(path, set_type)
+
+        #concat diff files labels #need revise  
+        data= globals()[str(set_type)+'_'+str(fnum)]
+        fnum+=1
+        
+
+        if '.mat' in path and 'y' in set_type:
+            if 'concat_data' not in globals():
+                concat_data= data.reshape(-1)
+            else:
+                concat_data= np.concatenate((concat_data, data.reshape(-1)))
+            
+            # find eventids from labels
+            for i in data.reshape(-1):
+                if i not in event_ids:
+                    event_ids.append(i)
+            event_ids.sort(reverse=False)
+
+            print(concat_data.shape)
+            
+        # display each file name
+        fname= path.split('/')[-1]
+        tk.Label(frame, text= fname, bg='White').pack(fill='both',padx=20)
+        subname.append( fname.split('.')[0])
+        sublen.append( len(data))
+
+    print(event_ids, subname, sublen)
+    
+def findevents(x):
+    return {
+        #'.set': data.event_id,
+        '.mat': event_ids,
+    }[x]
+
+def rename_events(w_files):
+
+    w_files.destroy()
+    w= new_window('Rename Events', '500x250', x_pad=-10, y_pad=180)
+    global table
+
+    # t_frame = tk.Frame(w, bg="White", bd=1, relief="sunken", width=450, height=50)
+    # t_frame.pack(side='top', fill='both',pady=30, ipady=20,ipadx=4)
+    print('len of eventtype', len(findevents(ftype)))
+    event_ids= findevents(ftype)
+
+    table= ttk.Treeview(w, height=len(event_ids))
+    
+    #create scroll bar 
+    sb = Scrollbar(w, orient=tk.VERTICAL)
+    sb.pack(side=tk.RIGHT, fill=tk.Y)
+    table.config(yscrollcommand=sb.set)
+    sb.config(command=table.yview)
+
+    table['column']= ['Event ID','Event type']
+    table.column('#0', width=0, stretch=tk.NO)
+    table.column('Event ID', anchor=tk.CENTER, width=30 )
+    table.column('Event type', anchor=tk.CENTER, width=130)
+    #table.column('Count', anchor=tk.CENTER, width=80)
+
+    table.heading('#0', text='', anchor=tk.CENTER)
+    table.heading('Event ID', text='Event ID', anchor=tk.CENTER)
+    table.heading('Event type', text='Event type', anchor=tk.CENTER)
+
+    if ftype== '.set': 
+        for row in range(len(event_ids)):
+            eventypes=list(event_ids.keys())
+            item= table.insert(parent='', index=row, iid= row, text='', values=[event_ids[eventypes[row]], eventypes[row] ])
+            table.item(item, tags=item)
+    if ftype== '.mat':
+        global eventype
+        eventype= [None]*len(event_ids) # eventype is none default in mat file 
+        for row in range(len(event_ids)):
+            item= table.insert(parent='', index=row, iid= row, text='', values=[event_ids[row], eventype[row] ])
+            table.item(item, tags=item)
+
+    table.bind('<1>', editname)
+    table.pack(side='top', fill='x',padx=10, pady=10)
+
+    Confirm = ttk.Button(w ,text="Confirm", command=(lambda:all_data(w)), width=10).pack()
+    # plot events
+    # figure = Figure(figsize=(5,5))
+    # ax = figure.add_subplot(1, 1, 1)
+    # mne.viz.plot_events(data.events, event_id=data.event_id, sfreq=562, axes=ax)
+    # canvas = FigureCanvasTkAgg(figure, w)
+    # canvas.get_tk_widget().pack(side='bottom')
+
+def editname(event):
+
+    if table.identify_region(event.x, event.y) == 'cell':
+        # the user clicked on a cell
+
+        def ok(event):
+            """Change item value."""
+            #print(entry.get(), type(entry.get()))
+            if column=='#1':
+                event_ids[int(item)]= int(entry.get())
+            if column=='#2':
+                eventype[int(item)]= entry.get()
+            print(event_ids, eventype)
+
+            table.set(item, column, entry.get())
+            entry.destroy()
+        
+        column = table.identify_column(event.x)  # identify column
+        item = table.identify_row(event.y)  # identify item
+        x, y, width, height = table.bbox(item, column) 
+        value = table.set(item, column)
+
+     
+        
+
+    elif table.identify_region(event.x, event.y) == 'heading': 
+        # the user clicked on a heading
+
+        def ok(event):
+            """Change heading text."""
+            table.heading(column, text=entry.get())
+            entry.destroy()
+
+        column = table.identify_column(event.x) # identify column
+        # tree.bbox work sonly with items so we have to get the bbox of the heading differently
+        x, y, width, _ = table.bbox(table.get_children('')[0], column) # get x and width (same as the one of any cell in the column)
+        # get vertical coordinates (y1, y2)
+        y2 = y
+        # get bottom coordinate
+        while table.identify_region(event.x, y2) != 'heading':  
+            y2 -= 1
+        # get top coordinate
+        y1 = y2
+        while table.identify_region(event.x, y1) == 'heading':
+            y1 -= 1
+        height = y2 - y1
+        y = y1
+        value = table.heading(column, 'text')
+
+    elif table.identify_region(event.x, event.y) == 'nothing': 
+        column = table.identify_column(event.x) # identify column
+        # check whether we are below the last row:
+        x, y, width, height =table.bbox(table.get_children('')[-1], column)
+        if event.y > y:
+
+            def ok(event):
+                """Change item value."""
+                # create item
+                item = table.insert("", "end", values=("", ""))
+                table.set(item, column, entry.get())
+                entry.destroy()
+
+            y += height
+            value = ""
+        else:
+            return
+    else:
+        return
+    # display the Entry   
+    entry = ttk.Entry(table)  # create edition entry
+    entry.place(x=x, y=y, width=width, height=height,
+                anchor='nw')  # display entry on top of cell
+    entry.insert(0, value)  # put former value in entry
+    entry.bind('<FocusOut>', lambda e: entry.destroy())  
+    entry.bind('<Return>', ok)  # validate with Enter
+    entry.focus_set()
+
+def all_data(w):
+    w.destroy()
+    w_data= new_window('Events ', '900x500', x_pad=-10, y_pad=100)
+
+    frame_allevents= tk.LabelFrame(w_data,text="All Events",bg="White", labelanchor='n')
+    frame_train= tk.LabelFrame(w_data, text="Training set",bg="White", labelanchor='n')
+    frame_test= tk.LabelFrame(w_data, text="Testing set",bg="White", labelanchor='n')
+
+    frame_allevents.grid(row=0, column=0)
+    frame_train.grid(row=0, column=1)
+    frame_test.grid(row=0, column=2)
+
+    
+    # .mat 
+    tabletype=['allevents', 'train', 'test']
+    tables= {}
+    for t in tabletype:
+        tables[t]= ttk.Treeview(locals()['frame_'+ t], height=20)
+        # globals()['table_'+ t]= ttk.Treeview(locals()['frame_'+ t], height=20)
+        # globals()['table_'+ t].grid()
+        #print(len(concat_data))
+        
+        #create scroll bar
+        sb = Scrollbar(locals()['frame_'+ t], orient=tk.VERTICAL)
+        sb.grid(row=0,column=1,sticky='ns')
+        tables[t].config(yscrollcommand=sb.set)
+        sb.config(command=tables[t].yview)
+        
+        #create columns
+        tables[t]['column']= ['Subject','Event ID','Event type','Onset time']
+        columns= tables[t]['column']
+
+        tables[t].column('#0', width=0, stretch=tk.NO)
+        tables[t].heading('#0', text='', anchor=tk.CENTER)
+        for col in columns:
+            tables[t].column(col, anchor=tk.CENTER, width=70 )
+            tables[t].heading(col, text='', anchor=tk.CENTER)
+
+        tables[t].grid(row=0, sticky= tk.N)
+        # table.column('Subject', anchor=tk.CENTER, width=30 )
+        # table.column('Event ID', anchor=tk.CENTER, width=30 )
+        # table.column('Event type', anchor=tk.CENTER, width=30)
+        # table.column('Onset time', anchor=tk.CENTER, width=50)
+
+        # table.heading('#0', text='', anchor=tk.CENTER)
+        # table.heading('Subject', text='Subject', anchor=tk.CENTER)
+        # table.heading('Event ID', text='Event ID', anchor=tk.CENTER)
+        # table.heading('Event type', text='Event type', anchor=tk.CENTER)
+        # table.heading('Onset time', text='Onset time', anchor=tk.CENTER)
+
+        if t=='allevents':
+            i=0
+            for row in range(len(concat_data)):
+                if row < sublen[i]:
+                    tables[t].insert(parent='', index=row, iid= row, text='', values=[subname[i], concat_data[row], eventype[concat_data[row]] , 'None'])
+                else:
+                    i+=1
+                    sublen[i]+= sublen[i-1]    
+
+            for col in columns:
+                tables[t].heading(col, text= col, command= lambda c=col: sort_id(tables[t], c, False))
+            i=0
+
+        elif t== 'train':
+            for row in range(5):
+                tables[t].insert(parent='', index=row, iid= row, text='', values=['ha', 'he', 'he' , 'ge'])
+            for col in columns:
+                tables[t].heading(col, text= col, command= lambda c=col: sort_id(tables[t], c, False))
+        
+        elif t== 'test':
+            for row in range(5):
+                tables[t].insert(parent='', index=row, iid= row, text='', values=['ha', 'he', 'he' , 'ge'])
+            for col in columns:
+                tables[t].heading(col, text= col, command= lambda c=col: sort_id(tables[t], c, False))
+    #ttk.Button(frame_allevents ,text="Move to training set", command=(Moveto(tables))).grid(row=1, column=0)
+    #ttk.Button(frame_allevents ,text="Move to test set", command=(Moveto(tables))).grid(row=1, column=1)
+    
+
+def Moveto(tables):
+    data= tk.Variable()
+    print([table['allevents'].item(idx) for idx in tables['allevents'].selection()])
+    #data.set([table['allevents'].item(idx) for idx in tables['allevents'].selection()])
+    
+
+
+def sort_id(table, col, reverse):
+    l = [(table.set(k, col), k) for k in table.get_children('')]
+    #print(l) #[(eventid,rowindex), ... ]
+    l.sort(key=lambda t: int(t[0]), reverse=reverse)
+    #print(l) #sort by eventid 
+
+    for index, (val, k) in enumerate(l): # save sorted changes to table 
+        table.move(item= k, parent= '', index= index) # move k to index position
+        table.heading(col,command=lambda: sort_id(table, col, False))
 
 # Menu- Model- Training setting =================================    
 
@@ -434,6 +746,7 @@ def show_para(win_p, Valratio, Batchsize, Epoch, Lr ):
     Model.entryconfig("Model Training", state="normal")
 
 def model_summary():
+    global output 
     net= scheme_var.netname
     dev = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
     old_stdout = sys.stdout
@@ -456,8 +769,18 @@ def model_summary():
 
     tk.Label(modelframe, text=trainable_para, bg= 'White').grid(row=9, column=1, sticky=tk.W)
     tk.Label(modelframe, text=memory_size, bg= 'White').grid(row=10, column=1, sticky=tk.W)
+    Model.entryconfig("Model Summary", state="normal")
 
-
+def full_model_summary():
+    w= new_window('Model Summary', size= '500x500', x_pad=-10, y_pad=180)
+    soutput= output.replace('=','').replace('-', '')
+    tk.Label(w, text= soutput, bg= 'White').grid()
+    #rows= output.split('\n')
+    # srows= [row.replace('=','').replace('-', '')+ '\n' for row in rows]
+    # i=0
+    # for row in soutput: 
+    #     tk.Label(w, text= row, bg= 'White').grid(row=i, column=0, sticky=tk.W)
+    #     i+=1
 # Menu- Model- Start training =================================
 
 def bar(): #w_bar, ypred, ypr remove from scheme_var? 
@@ -507,6 +830,7 @@ def check_train_thread():
         progressbar.stop()
 
         global conv1, conv2
+        print(scheme_var.saveweightfolder)
         conv1,conv2= get_weight(filepath=f'{scheme_var.saveweightfolder}/temp.pth')
         shortloadweightfile.set(f'{shortfolder}/temp.pth')
         tk.Label(modelframe, textvariable= shortloadweightfile, bg= 'White').grid(row=7, column=1, sticky=tk.W) 
