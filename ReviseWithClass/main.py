6f04856979c3eace70fae5546ec9f6cd8cbe1cd5
CECNL <90022378+CECNL@users.noreply.github.com>
2022-01-13 13:26:12 +0800

    Add files via upload

---
+++ ReviseWithClass/main.py
@@ -0,0 +1,9 @@
+import exbrainable
+
+def main():
+    GUI = exbrainable.ExBrainable()
+    GUI.execution()
+
+
+if __name__ == '__main__':
+    main() 
---
+++ ReviseWithClass/panel.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+import io
+import sys
+
+import window
+
+
+
+class Model_Summary_Panel():
+    def __init__(self):
+        pass
+
+
+
+class Load_Structure_Panel():
+    def __init__(self):
+        pass
+
+
+
+class Model_Setting_Panel():
+    def __init__(self):
+        pass
+
+
+
+class Load_Data_Panel():
+    def __init__(self):
+        pass
+
+
+
+class Load_Weight_Panel():
+    def __init__(self):
+        pass
+
+
+
+class Training_Panel():
+    def __init__(self):
+        pass
+
+
+
+class Predicted_Panel():
+    def __init__(self):
+        pass
+
+
+
+class Performance_Panel():
+    def __init__(self):
+        pass
+
+
+
+class Visualization_Panel():
+    def __init__(self):
+        pass
